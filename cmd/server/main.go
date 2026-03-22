4ec015545c1043636208627d2c0ec8d55ad5d462
scarypuppp <mazurash-works@mail.ru>
2026-03-22 21:52:12 +0300

    iter1 WIP

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleMetric(w http.ResponseWriter, req *http.Request) {
+	metricType := req.PathValue("metricType")
+	metricName := req.PathValue("metricName")
+	metricValue := req.PathValue("metricValue")
+
+	fmt.Println(metricType, metricName, metricValue)
+}
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/update/{metricType}/{metricName}/{metricValue}", handleMetric)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
